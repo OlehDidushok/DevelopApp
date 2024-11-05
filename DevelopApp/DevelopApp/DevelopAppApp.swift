e7685e762f4d4ee92212fdafe31db2d7c68da05a
Oleh Didushok <didushokov@gmail.com>
2024-11-05 12:53:03 +0200

    Created swiftui progect

---
+++ DevelopApp/DevelopApp/DevelopAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevelopAppApp.swift
+//  DevelopApp
+//
+//  Created by Oleh on 05.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct DevelopAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
