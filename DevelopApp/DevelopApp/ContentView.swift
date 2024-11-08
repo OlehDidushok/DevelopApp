c02af31344acfc21b1ee082c2a8ee92564ad655b
Oleh Didushok <didushokov@gmail.com>
2024-11-08 11:03:26 +0200

    Created CardView

--- DevelopApp/DevelopApp/ContentView.swift
+++ DevelopApp/DevelopApp/ContentView.swift
@@ -8,14 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // MARK: - Content
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        CardView()
     }
 }
 
